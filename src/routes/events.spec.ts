f8cdd6fe871d389559fdde8f508ec503f0fb2072
Kalle Lämsä <1397804+coocos@users.noreply.github.com>
2021-03-02 18:04:03 +0200

    Move type definition

--- src/routes/events.spec.ts
+++ src/routes/events.spec.ts
@@ -1,8 +1,14 @@
 import app from "../app";
+import * as eventRepo from "../repos/eventRepo";
 import request from "supertest";
 
 describe("/events", () => {
   it("returns list of events", async () => {
+    eventRepo.add({
+      type: "rakennuspalo: keskisuuri",
+      location: "Tuusula",
+      time: new Date("2021-01-31T22:00:00.000Z"),
+    });
     const response = await request(app).get("/api/v1/events");
     expect(response.status).toBe(200);
     expect(response.body).toEqual([
