9ed525453941502faf29a4936269adb88a884137
Kalle Lämsä <1397804+coocos@users.noreply.github.com>
2021-03-11 18:34:45 +0200

    Log objects fully

--- src/logger/index.ts
+++ src/logger/index.ts
@@ -3,6 +3,7 @@ import winston from "winston";
 export default winston.createLogger({
   format: winston.format.combine(
     winston.format.timestamp(),
+    winston.format.splat(),
     winston.format.colorize(),
     winston.format.printf(
       (info) => `${info.timestamp} ${info.level} ${info.message}`
