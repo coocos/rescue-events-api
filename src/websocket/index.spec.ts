4aff9e5e6660d954bf68d26187a1e133227fcb3f
Kalle Lämsä <1397804+coocos@users.noreply.github.com>
2021-03-15 18:23:14 +0200

    Test WebSocket broadcasting

---
+++ src/websocket/index.spec.ts
@@ -0,0 +1,44 @@
+import http from "http";
+import WebSocket, { AddressInfo } from "ws";
+
+import webSocketServer from ".";
+
+describe("WebSocket server", () => {
+  let server: http.Server;
+
+  beforeAll((done) => {
+    server = http.createServer();
+    server.listen(8000, "localhost");
+    server.on("listening", done);
+  });
+
+  it("broadcasts events to clients", (done) => {
+    const broadcast = webSocketServer(server);
+    const { address, port } = server.address() as AddressInfo;
+
+    const client = new WebSocket(`ws://${address}:${port}/websocket`);
+    client.on("open", () => {
+      broadcast({
+        type: "rakennuspalo: keskisuuri",
+        location: "Tuusula",
+        time: new Date("2021-01-31T22:00:00.000Z"),
+        hash: "2a39407ee0570aae8f3ba2842e11aa28ce0f5d9f",
+      });
+    });
+    client.on("message", (data) => {
+      const message = JSON.parse(data.toString());
+      expect(message).toEqual({
+        type: "rakennuspalo: keskisuuri",
+        location: "Tuusula",
+        time: "2021-01-31T22:00:00.000Z",
+        hash: "2a39407ee0570aae8f3ba2842e11aa28ce0f5d9f",
+      });
+      client.close();
+      done();
+    });
+  });
+
+  afterAll((done) => {
+    server.close(done);
+  });
+});
