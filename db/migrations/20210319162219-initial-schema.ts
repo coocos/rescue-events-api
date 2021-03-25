d0809e08a9039e1f5029912e6997ea8eab0985b3
Kalle Lämsä <1397804+coocos@users.noreply.github.com>
2021-03-25 19:19:54 +0200

    Make all columns non-nullable

--- db/migrations/20210319162219-initial-schema.ts
+++ db/migrations/20210319162219-initial-schema.ts
@@ -3,22 +3,22 @@ import { Knex } from "knex";
 export async function up(knex: Knex): Promise<void> {
   await knex.schema.createTable("locations", (table) => {
     table.increments();
-    table.string("name");
+    table.string("name").notNullable();
     table.unique(["name"]);
   });
   await knex.schema.createTable("types", (table) => {
     table.increments();
-    table.string("name");
+    table.string("name").notNullable();
     table.unique(["name"]);
   });
   await knex.schema.createTable("events", (table) => {
     table.increments();
-    table.integer("location_id").unsigned();
+    table.integer("location_id").unsigned().notNullable();
     table.foreign("location_id").references("locations.id");
-    table.integer("type_id").unsigned();
+    table.integer("type_id").unsigned().notNullable();
     table.foreign("type_id").references("types.id");
-    table.timestamp("time");
-    table.string("hash");
+    table.timestamp("time").notNullable();
+    table.string("hash").notNullable();
     table.unique(["hash"]);
   });
 }
